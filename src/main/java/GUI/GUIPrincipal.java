8aff90bfb901156fa695500c0599c778c9d95e3d
Admin <Admin@DESKTOP-8S6S2G8>
2026-09-25 21:40:38 -0500

    Proyecto de nuevo VERSION PRINCIPAL

--- src/main/java/GUI/GUIPrincipal.java
+++ src/main/java/GUI/GUIPrincipal.java
@@ -71,7 +71,6 @@ public class GUIPrincipal extends javax.swing.JFrame {
         lblEliminar = new javax.swing.JMenuItem();
         lblListar = new javax.swing.JMenuItem();
         lblPrecioTotal = new javax.swing.JMenuItem();
-        lblAdicionar1 = new javax.swing.JMenuItem();
         Ayuda = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
 
@@ -174,10 +173,6 @@ public class GUIPrincipal extends javax.swing.JFrame {
         lblPrecioTotal.setText("PrecioTotal");
         lblPrecioTotal.addActionListener(this::lblPrecioTotalActionPerformed);
         jMenu2.add(lblPrecioTotal);
-
-        lblAdicionar1.setText("PRUEBA");
-        lblAdicionar1.addActionListener(this::lblAdicionar1ActionPerformed);
-        jMenu2.add(lblAdicionar1);
 
         jMenuBar1.add(jMenu2);
 
@@ -366,13 +361,6 @@ public class GUIPrincipal extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_lblPrecioTotalActionPerformed
 
-    private void lblAdicionar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lblAdicionar1ActionPerformed
-        asdad ventana = new asdad();
-        ventana.setLocationRelativeTo(this);
-        ventana.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        ventana.setVisible(true);
-    }//GEN-LAST:event_lblAdicionar1ActionPerformed
-
     /**
      * S
      *
@@ -415,7 +403,6 @@ public class GUIPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem lblActualizar;
     private javax.swing.JMenuItem lblActualizar1;
     private javax.swing.JMenuItem lblAdicionar;
-    private javax.swing.JMenuItem lblAdicionar1;
     private javax.swing.JMenuItem lblAdicionarConcesionario;
     private javax.swing.JMenuItem lblBuscar;
     private javax.swing.JMenuItem lblBuscar1;
